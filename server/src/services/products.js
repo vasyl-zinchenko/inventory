46d40ac1ad540b9e86f66fa79c37e794234b7fc7
Vasyl Zinchenko <53319174+VasylZinchenko@users.noreply.github.com>
2022-12-14 19:21:21 +0200

    Update products.js

--- server/src/services/products.js
+++ server/src/services/products.js
@@ -49,31 +49,3 @@ export function removeProduct(productId) {
     where: { id: productId },
   });
 }
-
-// {
-//   "id": 1,
-//   "title": "Product 1",
-//   "isNew": 1,
-//   "serialNumber": "234539003",
-//   "photo": "pathToFile.jpg",
-//   "type": "Monitors",
-//   "specification": "Gigabyte Technology X58-USB3 (Socket 1366) 6 x58-USB3",
-//   "guarantee": {
-//       "end": "2017-06-29 12:09:33",
-//       "start": "2017-06-29 12:09:33"
-//   },
-//   "price": [
-//       {
-//           "value": 100,
-//           "symbol": "USD",
-//           "isDefault": 0
-//       },
-//       {
-//           "value": 2600,
-//           "symbol": "UAH",
-//           "isDefault": 1
-//       }
-//   ],
-//   "order": 10,
-//   "date": "2022-11-30T04:22:44.167Z"
-// },
