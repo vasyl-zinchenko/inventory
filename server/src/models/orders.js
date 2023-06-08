38101eefd2b6a4e25ce6882ae7073dc6a577d5ca
Vasyl Zinchenko <53319174+vasyl-zinchenko@users.noreply.github.com>
2023-06-08 17:41:15 +0300

    Update orders.js

--- server/src/models/orders.js
+++ server/src/models/orders.js
@@ -1,25 +1,3 @@
-// const {
-//   Model,
-// } = require('sequelize');
-
-// module.exports = (sequelize, DataTypes) => {
-//   class Orders extends Model {
-//     static associate(models) {
-//     }
-//   }
-
-//   Orders.init({
-//     title: DataTypes.STRING,
-//     description: DataTypes.STRING,
-//     date: DataTypes.DATE,
-//   }, {
-//     sequelize,
-//     modelName: 'orders',
-//   });
-
-//   return Orders;
-// };
-
 import {sequelize} from '../utils/db.js';
 import { DataTypes } from 'sequelize';
 
