64dacd4eb84cdfb71781499ff348f87af5e41f70
Vasyl Zinchenko <53319174+vasyl-zinchenko@users.noreply.github.com>
2023-06-08 17:40:31 +0300

    Update db.js

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,10 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-// export const sequelize = new Sequelize('dzencode', 'postgres', 'test124', {
-//   host: 'localhost',
-//   dialect: 'postgres',
-// });
-
 export const sequelize = new Sequelize('ddzghjrh',
   'ddzghjrh', 'JAp3TzLN44TgUFSu78_5iBkjRDlFz8xa', {
   host: 'lucky.db.elephantsql.com',
